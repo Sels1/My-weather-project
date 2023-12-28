b192a5a913c0cc951c0998eabdaa1a982ff96995
Selam <selamabrahamx@gmail.com>
2023-12-28 22:05:50 +0000

    Added more HTML, CSS and JS

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time = 1000);
+  let iconElement = document.querySelector("$icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
-  timeElement.innerHTML = "Tuesday 14:49";
   descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = '${response.data.temperature.humidity}%';
+  windSpeedElement.innerHTML = '${response.data.wind.speed}km/h';
   temperatureElement.innerHTML = Math.round(temperature);
+  icon.innerHTML =`<img src="${response.data.condition.icon_url}" class="weather-app-icon" />;
 }
+
 function formatDate(date) {
-  let day = date.getDay();
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days = [
@@ -31,18 +33,15 @@ function formatDate(date) {
   let day = days[date.getDay()];
 
   if (minutes < 10) {
-    minutes = `0${minutes}`;
+    minutes = 0${minutes};
   }
 
-  return `${day} ${hours}:${minutes}`;
+   return '${day} ${hours}:${minutes}';
 }
-
-
-
 
 function searchCity(city) {
   let apiKey = "a1afo4fc676594305dda8b00t8ea81fe";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}';
   axios.get(apiUrl).then(refreshWeather);
 }
 
