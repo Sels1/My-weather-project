bcd3222b9236e7472fb2445a6dc1721821eb4b29
Selam <selamabrahamx@gmail.com>
2023-12-28 21:02:16 +0000

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,43 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let windSpeedElement = document.querySelector("wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time = 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  timeElement.innerHTML = "Tuesday 14:49";
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
+function formatDate(date) {
+  let day = date.getDay();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+
+
 
 function searchCity(city) {
   let apiKey = "a1afo4fc676594305dda8b00t8ea81fe";
